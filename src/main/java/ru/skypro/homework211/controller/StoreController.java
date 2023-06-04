06b29b1376350fafc6db3633dc150f74394b3ae8
larchick <liliyagreis@gmail.com>
2023-06-04 19:38:51 +0300

    1

---
+++ src/main/java/ru/skypro/homework211/controller/StoreController.java
@@ -0,0 +1,4 @@
+package src.main.java.ru.skypro.homework211.controller;
+
+public class StoreController {
+}
---
+++ src/test/java/ru/skypro/homework211/Homework211ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework211;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework211ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
